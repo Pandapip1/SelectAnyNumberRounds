5689bdeb078af66fc0a982529036874d0a3d9e09
Pandapip1 <gavinnjohn@gmail.com>
2023-04-10 16:08:36 -0400

    Gotta love patching coroutines

--- patches/RespawnCardChoice.cs
+++ patches/RespawnCardChoice.cs
@@ -1,5 +1,7 @@
 using HarmonyLib;
 using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace SelectAnyNumberRounds.Patch
@@ -7,59 +9,86 @@ namespace SelectAnyNumberRounds.Patch
     [HarmonyPatch(typeof(CardChoice), nameof(CardChoice.IDoEndPick))]
     public static class RespawnCardChoice
     {
-        public static bool Prefix(GameObject pickedCard, ref List<GameObject> __state, ref CardChoice __instance, ref List<GameObject> ___spawnedCards, ref int ___currentlySelectedCard)
+        // This class forked from https://github.com/willuwontu/WillsWackyGamemodes/blob/main/WillsWackyGamemodes/Patches/CardChoice_Patch.cs
+        class SimpleEnumerator : IEnumerable
         {
-            // If the player picked no card, return true to allow the original method to run
-            if (!pickedCard)
+            public IEnumerator enumerator;
+            public Action prefixAction, postfixAction;
+            public Action<object> preItemAction, postItemAction;
+            public Func<object, object> itemAction;
+            IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+            public IEnumerator GetEnumerator()
             {
-                __state = null;
-                return true;
+                prefixAction();
+                while (enumerator.MoveNext())
+                {
+                    var item = enumerator.Current;
+                    preItemAction(item);
+                    yield return itemAction(item);
+                    postItemAction(item);
+                }
+                postfixAction();
             }
-
-            // If the player picked the continue card, clear the list of cards as usual
-            if (pickedCard.name.Contains("Continue"))
-            {
-                __state = null;
-                return true;
-            }
-
-            // Otherwise, partially replace the original method
-
-            // Display the card as picked
-            pickedCard.GetComponentInChildren<CardVisuals>().Pick();
-
-            // Remove the card from the list of spawned cards
-            ___spawnedCards.Remove(pickedCard);
-
-            // Update the ints
-            for (int i = 0; i < ___spawnedCards.Count; i++)
-            {
-                ___spawnedCards[i].GetComponentInChildren<PublicInt>().theInt = i;
-            }
-
-            // Set the state to the list of spawned cards
-            __state = ___spawnedCards;
-
-            // Set the list of spawned cards to the list containing only the picked card
-            ___spawnedCards = new List<GameObject>() { pickedCard };
-
-            // Update the currently selected card
-            ___currentlySelectedCard = 0;
-
-            // The original method will now run, but it will only pick the card that was picked, and won't destroy the other cards
-            return true;
         }
 
-        public static void Postfix(ref List<GameObject> __state, ref List<GameObject> ___spawnedCards)
+        public static void Postfix(GameObject pickedCard, ref IEnumerator __result, ref List<GameObject> ___spawnedCards)
         {
-            // If the state is null or empty, return
-            if (__state == null || __state.Count == 0)
-            {
-                return;
-            }
+            var justPickedCards = new List<GameObject>() { pickedCard };
+            var spawnedCardsSave = ___spawnedCards;
 
-            // Otherwise, restore the list of spawned cards
-            ___spawnedCards = __state;
+            // If the state is null or empty, return
+            var myEnumerator = new SimpleEnumerator()
+            {
+                enumerator = __result,
+                prefixAction = () => { },
+                postfixAction = () => {
+                    if (!pickedCard || pickedCard.name.Contains("Continue"))
+                    {
+                        return;
+                    }
+
+                    // Get using reflection
+                    var spawnedCards = spawnedCardsSave;
+
+                    // Remove the card from the list of spawned cards
+                    spawnedCards.Remove(pickedCard);
+
+                    // Update the ints
+                    for (int i = 0; i < spawnedCards.Count; i++)
+                    {
+                        spawnedCards[i].GetComponentInChildren<PublicInt>().theInt = i;
+                    }
+
+                    // Set using reflection
+                    typeof(CardChoice).GetField("spawnedCards", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(CardChoice.instance, spawnedCards);
+
+                    // Set currently selected card
+                    typeof(CardChoice).GetField("currentlySelectedCard", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(CardChoice.instance, 0);
+                    
+                    // Destroy the card
+                    pickedCard.GetComponentInChildren<CardVisuals>().Pick();
+                },
+                preItemAction = item => {
+                    if (!pickedCard || pickedCard.name.Contains("Continue"))
+                    {
+                        return;
+                    }
+
+                    // Set using reflection
+                    typeof(CardChoice).GetField("spawnedCards", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(CardChoice.instance, justPickedCards);
+                },
+                postItemAction = item => {
+                    if (!pickedCard || pickedCard.name.Contains("Continue"))
+                    {
+                        return;
+                    }
+
+                    // Set using reflection
+                    typeof(CardChoice).GetField("spawnedCards", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(CardChoice.instance, spawnedCardsSave);
+                },
+                itemAction = item => item
+            };
+            __result = myEnumerator.GetEnumerator();
         }
     }
 }
