9956376c6d1503bff2c4e68411909a7df7b7c6ec
Pandapip1 <gavinnjohn@gmail.com>
2023-04-13 16:55:49 -0400

    Fix config setting

--- Plugin.cs
+++ Plugin.cs
@@ -13,12 +13,16 @@ namespace SelectAnyNumberRounds
         public static Plugin instance;
         public static new ManualLogSource Logger => Plugin.instance.GetLogger();
 
-        public static ConfigEntry<bool> configUnlimitedPicks => instance.Config.Bind(PluginInfo.PLUGIN_GUID, "Unlimited Picks", true, "If true, you can pick as many cards as you want from each hand. If false, you can only pick the number of cards specified in the 'Picks' setting.");
-        public static ConfigEntry<int> configPickNumber => instance.Config.Bind(PluginInfo.PLUGIN_GUID, "Picks", 1, "The number of cards you can pick from each hand.");
+        public static ConfigEntry<bool> configUnlimitedPicks;
+        public static ConfigEntry<int> configPickNumber;
         
         private void Awake()
         {
             instance = this;
+
+            // Config
+            configUnlimitedPicks = instance.Config.Bind(PluginInfo.PLUGIN_GUID, "Unlimited Picks", true, "If true, you can pick as many cards as you want from each hand. If false, you can only pick the number of cards specified in the 'Picks' setting.");
+            configPickNumber = instance.Config.Bind(PluginInfo.PLUGIN_GUID, "Picks", 1, "The number of cards you can pick from each hand.");
 
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
