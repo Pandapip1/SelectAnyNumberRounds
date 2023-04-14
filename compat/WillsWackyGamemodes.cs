b6aa19f5639f92b31831b06ee0bae5cd3015b75c
Pandapip1 <gavinnjohn@gmail.com>
2023-04-14 10:44:00 -0400

    Better draft compatibility

--- compat/WillsWackyGamemodes.cs
+++ compat/WillsWackyGamemodes.cs
@@ -9,21 +9,26 @@ namespace SelectAnyNumberRounds.Compat
     [BepInPlugin("com.pandapip1.rounds.selectanynumberrounds.compat.willswackygmcompat", "Select Any Number Rounds - Will's Wacky Gamemodes Compatibility", PluginInfo.PLUGIN_VERSION)]
     public class WillsWackyGMCompat : BaseUnityPlugin
     {
+        public static bool oldIsDraft = false;
         public static int oldConfigPickNumber = 0;
+        public static bool oldEnableContinueCard = false;
         public virtual void Update()
         {
-            if (GameModeManager.CurrentHandlerID == "Draft")
+            if (GameModeManager.CurrentHandlerID == "Draft" && !oldIsDraft)
             {
-                if (Plugin.configPickNumber.Value != 1)
-                {
-                    oldConfigPickNumber = Plugin.configPickNumber.Value;
-                    Plugin.configPickNumber.Value = 1; // Draft only allows 1 pick
-                }
-            } else if (oldConfigPickNumber != 0)
+                oldConfigPickNumber = Plugin.configPickNumber.Value;
+                oldEnableContinueCard = Plugin.enableContinueCard.Value;
+                Plugin.configPickNumber.Value = 1; // Draft only allows 1 pick
+                Plugin.enableContinueCard.Value = false; // Draft doesn't allow continue cards
+            }
+            if (GameModeManager.CurrentHandlerID != "Draft" && oldIsDraft)
             {
                 Plugin.configPickNumber.Value = oldConfigPickNumber;
+                Plugin.enableContinueCard.Value = oldEnableContinueCard;
                 oldConfigPickNumber = 0;
+                oldEnableContinueCard = false;
             }
+            oldIsDraft = GameModeManager.CurrentHandlerID == "Draft";
         }
     }
 }
